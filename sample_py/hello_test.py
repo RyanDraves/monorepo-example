c949bc349b3c56de21efdb53ae185f779d13f160
Ryan Draves <dravesr@umich.edu>
2023-03-30 18:24:33 -0700

    add setup.py, fix things

--- sample_py/hello_test.py
+++ sample_py/hello_test.py
@@ -4,6 +4,6 @@ from sample_py import hello
 
 class HelloTest(unittest.TestCase):
   def test_hello(self):
-    self.assertEquals(hello.hello(2), 'hellohello')
-    self.assertEquals(hello.hello(1), 'hello')
-    self.assertEquals(hello.hello(0), '')
+    self.assertEqual(hello.hello(2), 'hellohello')
+    self.assertEqual(hello.hello(1), 'hello')
+    self.assertEqual(hello.hello(0), '')
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='SamplePy',
+      version='1.0',
+      description='Sample Python package',
+      author='Ryan Draves',
+      packages=['sample_py'],
+     )
